0e9d3a4b52f635542b7790eaf234b0a8ad3353b9
Keshav Krishn <keshavkrishn1006@gmail.com>
2025-08-03 19:17:37 +0530

    added kafka consumer

---
+++ analytics-service/src/main/java/org/chrisblakely/analyticsservice/kafka/KafkaConsumer.java
@@ -0,0 +1,45 @@
+package org.chrisblakely.analyticsservice.kafka;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.protobuf.InvalidProtocolBufferException;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+import org.springframework.kafka.annotation.KafkaListener;
+import patient.events.PatientEvent;
+
+@Service
+public class KafkaConsumer {
+
+    private static final Logger log = LoggerFactory.getLogger(
+            KafkaConsumer.class);
+//
+//    @KafkaListener(topics="patient", groupId = "analytics-service")
+//    public void consumeEvent(byte[] event) {
+//        try {
+//            PatientEvent patientEvent = PatientEvent.parseFrom(event);
+//            // ... perform any business related to analytics here
+//
+//            log.info("Received Patient Event: [PatientId={},PatientName={},PatientEmail={}]",
+//                    patientEvent.getPatientId(),
+//                    patientEvent.getName(),
+//                    patientEvent.getEmail());
+//        } catch (InvalidProtocolBufferException e) {
+//            log.error("Error deserializing event {}", e.getMessage());
+//        }
+//    }
+
+    @KafkaListener(topics = "patient", groupId = "analytics-service")
+    public void consumeEvent(ConsumerRecord<String, byte[]> record){
+        byte[] event = record.value();
+
+        try {
+            PatientEvent patientEvent = PatientEvent.parseFrom(event);
+            log.info("Received patient event: [PatientId={}, PatientName={}, PatientEmail={}]",
+                    patientEvent.getPatientId(), patientEvent.getName(), patientEvent.getEmail());
+        } catch (InvalidProtocolBufferException e) {
+            log.error("Error deserializing event: {}", e.getMessage());
+        }
+    }
+}
