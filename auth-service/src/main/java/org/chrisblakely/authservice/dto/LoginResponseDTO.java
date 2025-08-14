e688b85a96856be88d09a021d611d672247ef933
Keshav Krishn <keshavkrishn1006@gmail.com>
2025-08-15 03:42:05 +0530

    created login response dto

---
+++ auth-service/src/main/java/org/chrisblakely/authservice/dto/LoginResponseDTO.java
@@ -0,0 +1,13 @@
+package org.chrisblakely.authservice.dto;
+
+public class LoginResponseDTO {
+    private final String token;
+
+    public LoginResponseDTO(String token) {
+        this.token = token;
+    }
+
+    public String getToken() {
+        return token;
+    }
+}
