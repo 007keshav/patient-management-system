1c5d0cee9810c7e12798afbea659d0caa0c7d39b
Keshav Krishn <keshavkrishn1006@gmail.com>
2025-08-15 03:43:02 +0530

    created a user repository class

---
+++ auth-service/src/main/java/org/chrisblakely/authservice/repository/UserRepository.java
@@ -0,0 +1,14 @@
+package org.chrisblakely.authservice.repository;
+
+import org.chrisblakely.authservice.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, UUID> {
+
+    Optional<User> findByEmail(String email);
+}
