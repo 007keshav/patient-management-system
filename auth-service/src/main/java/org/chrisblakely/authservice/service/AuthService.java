8791ed0d43dac03ffe5af0f49c743f6f88af2dc2
Keshav Krishn <keshavkrishn1006@gmail.com>
2025-08-16 14:40:47 +0530

    validate token added

--- auth-service/src/main/java/org/chrisblakely/authservice/service/AuthService.java
+++ auth-service/src/main/java/org/chrisblakely/authservice/service/AuthService.java
@@ -1,6 +1,7 @@
 package org.chrisblakely.authservice.service;
 
 
+import io.jsonwebtoken.JwtException;
 import org.chrisblakely.authservice.dto.LoginRequestDTO;
 import org.chrisblakely.authservice.dto.LoginResponseDTO;
 import org.chrisblakely.authservice.model.User;
@@ -17,19 +18,28 @@ public class AuthService {
     private final PasswordEncoder passwordEncoder;
     private final JwtUtil jwtUtil;
 
-    public AuthService(UserService userService, PasswordEncoder passwordEncoder,  JwtUtil jwtUtil) {
+    public AuthService(UserService userService, PasswordEncoder passwordEncoder,
+                       JwtUtil jwtUtil) {
         this.userService = userService;
         this.passwordEncoder = passwordEncoder;
         this.jwtUtil = jwtUtil;
     }
 
-
     public Optional<String> authenticate(LoginRequestDTO loginRequestDTO) {
-        Optional<String>  token = userService.findByEmail(loginRequestDTO.getEmail())
-                .filter(u -> passwordEncoder.matches(loginRequestDTO.getPassword(), u.getPassword()))
-                .map(u->jwtUtil.generateToken(u.getEmail(),u.getRole()));
+        Optional<String> token = userService.findByEmail(loginRequestDTO.getEmail())
+                .filter(u -> passwordEncoder.matches(loginRequestDTO.getPassword(),
+                        u.getPassword()))
+                .map(u -> jwtUtil.generateToken(u.getEmail(), u.getRole()));
 
         return token;
+    }
 
+    public boolean validateToken(String token) {
+        try {
+            jwtUtil.validateToken(token);
+            return true;
+        } catch (JwtException e){
+            return false;
+        }
     }
 }
